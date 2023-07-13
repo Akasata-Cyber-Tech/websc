f6fb6ae6a255768715895ed989aa5b76f72c0a3f
Risqiyyyy <raisafebriani05@gmail.com>
2023-07-13 11:28:02 +0700

    dashboard

---
+++ app/Views/partials/sidebar.php
@@ -0,0 +1,94 @@
+		<div id="sidebar" class="app-sidebar">
+			<!-- BEGIN scrollbar -->
+			<div class="app-sidebar-content" data-scrollbar="true" data-height="100%">
+				<!-- BEGIN menu -->
+				<div class="menu">
+					<div class="menu-profile">
+						<a href="javascript:;" class="menu-profile-link" data-toggle="app-sidebar-profile" data-target="#appSidebarProfileMenu">
+							<div class="menu-profile-cover with-shadow"></div>
+							<div class="menu-profile-image">
+								<img src="../assets/img/user/user-13.jpg" alt="" />
+							</div>
+							<div class="menu-profile-info">
+								<div class="d-flex align-items-center">
+									<div class="flex-grow-1">
+										Sean Ngu
+									</div>
+									<div class="menu-caret ms-auto"></div>
+								</div>
+								<small>Frontend developer</small>
+							</div>
+						</a>
+					</div>
+					<div id="appSidebarProfileMenu" class="collapse">
+						<div class="menu-item pt-5px">
+							<a href="javascript:;" class="menu-link">
+								<div class="menu-icon"><i class="fa fa-cog"></i></div>
+								<div class="menu-text">Settings</div>
+							</a>
+						</div>
+						<div class="menu-item">
+							<a href="javascript:;" class="menu-link">
+								<div class="menu-icon"><i class="fa fa-pencil-alt"></i></div>
+								<div class="menu-text"> Send Feedback</div>
+							</a>
+						</div>
+						<div class="menu-item pb-5px">
+							<a href="javascript:;" class="menu-link">
+								<div class="menu-icon"><i class="fa fa-question-circle"></i></div>
+								<div class="menu-text"> Helps</div>
+							</a>
+						</div>
+						<div class="menu-divider m-0"></div>
+					</div>
+					<div class="menu-header">Pages</div>
+					<div class="menu-item has-sub active">
+                    <div class="menu-item ">
+						<a href="<?php echo site_url('/') ?>" class="menu-link">
+							<div class="menu-icon-img">
+								<img src="../assets/img/logo/logo-bs5.png" alt="" />
+							</div>
+							<div class="menu-text">Dashboard</div> 
+						</a>
+					</div>
+                    <div class="menu-item ">
+						<a href="bootstrap_5.html" class="menu-link">
+							<div class="menu-icon-img">
+								<img src="../assets/img/logo/logo-bs5.png" alt="" />
+							</div>
+							<div class="menu-text">Assets</div> 
+						</a>
+					</div>
+                    
+                    <div class="menu-header">Contol Panel</div>
+                    <div class="menu-item ">
+						<a href="bootstrap_5.html" class="menu-link">
+							<div class="menu-icon-img">
+								<img src="../assets/img/logo/logo-bs5.png" alt="" />
+							</div>
+							<div class="menu-text">Users</div> 
+						</a>
+					</div>
+                    <div class="menu-item ">
+						<a href="bootstrap_5.html" class="menu-link">
+							<div class="menu-icon-img">
+								<img src="../assets/img/logo/logo-bs5.png" alt="" />
+							</div>
+							<div class="menu-text">Data Permissions</div> 
+						</a>
+					</div>
+                    <div class="menu-item ">
+						<a href="bootstrap_5.html" class="menu-link">
+							<div class="menu-icon-img">
+								<img src="../assets/img/logo/logo-bs5.png" alt="" />
+							</div>
+							<div class="menu-text">Roles</div> 
+						</a>
+					</div>
+					</div>
+				</div>
+				<!-- END menu -->
+			</div>
+			<!-- END scrollbar -->
+		</div>
+		<!-- END #sidebar -->
--- vendor/codeigniter4/shield/src/Views/layout.php
+++ vendor/codeigniter4/shield/src/Views/layout.php
@@ -26,37 +26,7 @@
     <main role="main" class="container">
         <?= $this->renderSection('main') ?>
     </main>
-<!-- ================== BEGIN core-js ================== -->
-<script src="../assets/js/vendor.min.js"></script>
-	<script src="../assets/js/app.min.js"></script>
-	<!-- ================== END core-js ================== -->
-	
-	<!-- ================== BEGIN page-js ================== -->
-	<script src="../assets/plugins/gritter/js/jquery.gritter.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.canvaswrapper.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.colorhelpers.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.saturated.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.browser.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.drawSeries.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.uiConstants.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.time.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.resize.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.pie.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.crosshair.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.categories.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.navigate.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.touchNavigate.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.hover.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.touch.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.selection.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.symbol.js"></script>
-	<script src="../assets/plugins/flot/source/jquery.flot.legend.js"></script>
-	<script src="../assets/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
-	<script src="../assets/plugins/jvectormap-next/jquery-jvectormap.min.js"></script>
-	<script src="../assets/plugins/jvectormap-content/world-mill.js"></script>
-	<script src="../assets/plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.js"></script>
-	<script src="../assets/js/demo/dashboard.js"></script>
+
 <?= $this->renderSection('pageScripts') ?>
 </body>
 </html>
