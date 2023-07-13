f3b009abdca96c2cc4b37b76c46015de76e2ce6f
Risqiyyyy <raisafebriani05@gmail.com>
2023-07-13 10:07:36 +0700

    login dan register

--- vendor/codeigniter4/shield/src/Views/register.php
+++ vendor/codeigniter4/shield/src/Views/register.php
@@ -3,12 +3,25 @@
 <?= $this->section('title') ?><?= lang('Auth.register') ?> <?= $this->endSection() ?>
 
 <?= $this->section('main') ?>
+<!-- register -->
+<div id="app" class="app">
+		<div class="login login-v2 fw-bold">
+			<div class="login-cover">
+				<div class="login-cover-img" style="background-image: url(../assets/img/login-bg/login-bg-17.jpg)" data-id="login-cover-image"></div>
+				<div class="login-cover-bg"></div>
+			</div>
+			
 
-    <div class="container d-flex justify-content-center p-5">
-        <div class="card col-12 col-md-5 shadow-sm">
-            <div class="card-body">
-                <h5 class="card-title mb-5"><?= lang('Auth.register') ?></h5>
-
+			<div class="login-container">
+				<div class="login-header">
+					<div class="brand">
+						<div class="d-flex align-items-center">
+							<span class="logo"></span>Register
+						</div>
+					</div>
+				</div>
+				
+				<div class="login-content">
                 <?php if (session('error') !== null) : ?>
                     <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
                 <?php elseif (session('errors') !== null) : ?>
@@ -24,38 +37,42 @@
                     </div>
                 <?php endif ?>
 
-                <form action="<?= url_to('register') ?>" method="post">
-                    <?= csrf_field() ?>
+					<form action="<?= url_to('register') ?>" method="POST">
+					<?= csrf_field() ?>
+                    <!-- email -->
+						<div class="form-floating mb-20px">
+							<input type="text" class="form-control fs-13px h-45px border-0" name="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required/>
+							<label class="d-flex align-items-center text-gray-600 fs-13px">Email Address</label>
+						</div>
 
-                    <!-- Email -->
-                    <div class="mb-2">
-                        <input type="email" class="form-control" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required />
-                    </div>
+                    <!-- username -->
+                    <div class="form-floating mb-20px">
+							<input type="text" class="form-control fs-13px h-45px border-0" name="username" placeholder="<?= lang('Auth.username') ?>" value="<?= old('username') ?>" required />
+							<label class="d-flex align-items-center text-gray-600 fs-13px">Username</label>
+					</div>
 
-                    <!-- Username -->
-                    <div class="mb-4">
-                        <input type="text" class="form-control" name="username" inputmode="text" autocomplete="username" placeholder="<?= lang('Auth.username') ?>" value="<?= old('username') ?>" required />
-                    </div>
+                    <!-- password -->
+						<div class="form-floating mb-20px">
+							<input type="password" class="form-control fs-13px h-45px border-0" name="password" placeholder="<?= lang('Auth.password') ?>"/>
+							<label class="d-flex align-items-center text-gray-600 fs-13px">Password</label>
+						</div>
+                        <div class="form-floating mb-20px">
+							<input type="password" class="form-control fs-13px h-45px border-0" name="password_confirm" placeholder="<?= lang('Auth.passwordConfirm') ?>"/>
+							<label class="d-flex align-items-center text-gray-600 fs-13px">Confirm Password</label>
+						</div>
 
-                    <!-- Password -->
-                    <div class="mb-2">
-                        <input type="password" class="form-control" name="password" inputmode="text" autocomplete="new-password" placeholder="<?= lang('Auth.password') ?>" required />
-                    </div>
+						<div class="mb-20px">
+							<button type="submit" name="submit" class="btn btn-success d-block w-100 h-45px btn-lg"><?= lang('Auth.register') ?></button>
+						</div>
 
-                    <!-- Password (Again) -->
-                    <div class="mb-5">
-                        <input type="password" class="form-control" name="password_confirm" inputmode="text" autocomplete="new-password" placeholder="<?= lang('Auth.passwordConfirm') ?>" required />
-                    </div>
+                        <p class="text-center"><?= lang('Auth.haveAccount') ?> <a href="<?= url_to('login') ?>"><?= lang('Auth.login') ?></a></p>
+					
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 
-                    <div class="d-grid col-12 col-md-8 mx-auto m-3">
-                        <button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.register') ?></button>
-                    </div>
-
-                    <p class="text-center"><?= lang('Auth.haveAccount') ?> <a href="<?= url_to('login') ?>"><?= lang('Auth.login') ?></a></p>
-
-                </form>
-            </div>
-        </div>
-    </div>
+<!-- end register -->
 
 <?= $this->endSection() ?>
